4a712fd561fc7bff501c4e9dd8fc9737405c889b
KW781 <59013794+KW781@users.noreply.github.com>
2021-04-10 04:32:15 +1200

    Add files via upload

---
+++ build.rs
@@ -0,0 +1,27 @@
+//currently, there is no accounting for whitelisting/blacklisting of particular functions/variables
+//this is likely to be implemented later on during GSoC
+
+extern crate cmake;
+extern crate bindgen;
+use std::path::PathBuf;
+use std::env;
+
+fn main() {
+    let lib = cmake::build("ccextractor/src"); //build lib_ccx using cmake
+
+    println!("cargo:rustc-link-search=native={}/lib", lib.display());
+    println!("cargo:rustc-link-lib=static=ccx");
+    println!("cargo:return-if-changed=wrapper.h"); //re-generate the bindings if there are any changes made to the wrapper file
+
+    let mut bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()); //write the bindings for the headers to the bindings.rs file
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Error with writing bindings");
+
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include "ccextractor/src/lib_ccx/lib_ccx.h"
