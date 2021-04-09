cb7b27e721e2be616296cbd86cb3f17a3deae4ff
KW781 <59013794+KW781@users.noreply.github.com>
2021-04-10 04:39:31 +1200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::process;
+use std::io::raw::{c_int, c_long};
+use std::mem::{self, MaybeUninit};
+use std::ffi::CString;
+use std::env;
+use std::convert::TryInto;
+
+//mod bindings;
+
+fn main() {
+    unsafe {
+        let mut args = env::args()
+            .map(|arg| CString::new(arg).map(CString::into_raw))
+            .collect::<Result<Vec<_>, _>>()
+            .expect("One or more of the arguments contains a null charactor");
+        let argc = args.len().try_into().expect("Can't convert argc into c_int type");
+        let argv = args.as_mut_ptr();
+
+        cc_extractor_bindings-sys::setlocale(LC_ALL, ""); //support for non-English CCs
+
+        let api_options = cc_extractor_bindings-sys::api_init_options();
+        cc_extractor_bindings-sys::parse_configuration(api_options);
+
+        let compile_ret = cc_extractor_bindings-sys::parse_parameters(api_options, argc, argv);
+
+        for pointer in args {
+            drop(CString::from_raw(ptr));
+        }
+
+        if compile_ret == EXIT_NO_INPUT_FILES as i32 {
+            cc_extractor_bindings-sys::print_usage();
+            fatal(EXIT_NO_INPUT_FILES, "(This help screen was shown because there were no input files)\n");
+        }
+        else if compile_ret == EXIT_WITH_HELP as i32{
+            process:exit(EXIT_OK);
+        }s
+        else if compile_ret != EXIT_OK as i32{
+            process::exit(compile_ret);
+        }
+
+        let start_ret = cc_extractor_bindings-sys::api_start(*api_options);
+        process::exit(start_ret);
+    }
+}
